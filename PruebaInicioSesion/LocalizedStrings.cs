fd0706e80cc58e68239ce8f9d21f6ce8c90a7dba
C354R <hsm21c354r@gmail.com>
2014-11-08 18:40:00 -0600

    Prueba

---
+++ PruebaInicioSesion/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using PruebaInicioSesion.Resources;
+
+namespace PruebaInicioSesion
+{
+    /// <summary>
+    /// Proporciona acceso a los recursos de cadena.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
