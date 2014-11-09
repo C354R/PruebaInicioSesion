d39dabd0045d7671e7040f21bc94c122cda1a4a3
C354R <hsm21c354r@gmail.com>
2014-11-08 20:32:10 -0600

    08/11

--- PruebaInicioSesion/MainPage.xaml.cs
+++ PruebaInicioSesion/MainPage.xaml.cs
@@ -22,20 +22,13 @@ namespace PruebaInicioSesion
             //BuildLocalizedApplicationBar();
         }
 
-        // Código de ejemplo para compilar una ApplicationBar traducida
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Establecer ApplicationBar de la página en una nueva instancia de ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
+        private void OnSessionStateChanged(object sender, Facebook.Client.Controls.SessionStateChangedEventArgs e)
+        {
+            this.ContentPanel.Visibility = (e.SessionState == Facebook.Client.Controls.FacebookSessionState.Opened) ?
+                Visibility.Visible : Visibility.Collapsed;
+        }
 
-        //    // Crear un nuevo botón y establecer el valor de texto en la cadena traducida de AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
 
-        //    // Crear un nuevo elemento de menú con la cadena traducida de AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
+       
     }
 }
